acb21ae21cd1e2fb0011c5d0576f1a5ee9c168fa
Pasindu De Silva <pasindudesilva695@gmail.com>
2024-12-22 18:58:21 -0500

    Update splitters

--- Data_Splitter.py
+++ Data_Splitter.py
@@ -25,37 +25,6 @@ def split_training_testing_samples(_dataset_file):
         writer.writerow(header)
         writer.writerows(testing_data)
 
-datasetFile = "Trnx_Dataset.csv"
-# split_training_testing_samples(datasetFile)
+datasetFile = "Trnx_Dataset _v2.csv"
+split_training_testing_samples(datasetFile)
 
-# Data cleaner
-
-hugeDataset = "G:\\My Drive\\College Stuff\\Capstone\\HI-Small_Trans.csv"
-outputCSV = "IBM_Dataset.csv"
-
-def work_the_data():
-    with open(hugeDataset, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        header = reader.fieldnames
-
-        laundering_rows = []
-        other_rows = []
-
-        for row in reader:
-            if row['Is Laundering'] == '1':
-                laundering_rows.append(row)
-            else:
-                other_rows.append(row)
-
-        random.shuffle(other_rows)
-
-        total_required = 700000 - len(laundering_rows)
-        additional_rows = other_rows[:total_required]
-        all_rows = laundering_rows + additional_rows
-
-        with open(outputCSV, 'w', newline='') as output_file:
-            writer = csv.DictWriter(output_file, fieldnames=header)
-            writer.writeheader()
-            writer.writerows(all_rows)
-
-work_the_data()
